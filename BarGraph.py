deef9fe0843512ac4a2f4d2cd71f36199e6af046
cynobill <no.spam.please.were.british@gmail.com>
2018-09-14 11:48:52 +0000

    Quick commit before leaving, basic functionality done

--- BarGraph.py
+++ BarGraph.py
@@ -1,8 +1,13 @@
+from random import randint
+import cairo 
+
 class BarGraph():
 	def __init__(self):
 		print("Bargraph initialized")
 		self._data = []
-		self._max_data_count = 10
+		self._source = None
+
+		self._max_data_count = 5
 		self.max_data_value = 100
 		self.bounds = {
 			'x': 50,
@@ -15,33 +20,47 @@ class BarGraph():
 	def _update(self):
 		if len(self._data) >= self._max_data_count:
 			self._data.pop(-1)
-		new_value = 20
+		new_value = randint(0,100)
 		self._data.insert(0, new_value) 
+	
+	def _get_source(self):
+		if self._source == None:
+			self._source = cairo.LinearGradient(self.bounds['x'],
+							self.bounds['y']+self.bounds['h'],
+							self.bounds['x'],							 self.bounds['y'])
+
+		self._source.add_color_stop_rgba(0.0,0.0,1.0,0.0,1.0)
+		self._source.add_color_stop_rgba(1.0,1.0,0.0,0.0,1.0)
+		return self._source
 
 	def draw(self, context):
 		self._update()
+		src = self._get_source()
+		
 		context.set_source_rgba(0.0,1.0,0.0,1.0)
+		context.set_source(src)
 		
 		base = self.bounds['y'] + self.bounds['h']
-		line_width = (self.bounds['x'] - (self.bar_spacing * self._max_data_count - 1)) # / self._max_data_count
-		print(str(line_width))
+		line_width = (self.bounds['w'] - (self.bar_spacing * (self._max_data_count - 1)))  / self._max_data_count
 
 		increment = line_width + self.bar_spacing		
 
 		context.set_line_width(line_width)
 		start = self.bounds['x'] + line_width / 2
 
-		print("start: "+str(start)+", width: "+str(line_width)+", base: "+str(base)+", inc="+str(increment))
-
 		for index, value in enumerate(self._data):
 			x = start + index * increment
 			h = value / self.max_data_value * self.bounds['h']
 			context.move_to(x, base)
 			context.line_to(x, base - h)
-			print(str(index)+": "+str(h))
 
 		context.stroke()
-			
-		#context.rectangle(10,10,200,200)
-		#context.fill()
+		
+		context.set_source_rgba(0.0,0.0,1.0,1.0)	
+		context.set_line_width(1)
+		context.rectangle(self.bounds['x'],
+				  self.bounds['y'],
+				  self.bounds['w'],
+				  self.bounds['h'])
+		context.stroke()
 		#print('+')
